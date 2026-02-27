5dd8840a289584c2a148e6f5b02d874642213f96
jarroy11 <jarroy11@live.spcollege.edu>
2026-02-27 15:34:55 -0500

    ch13 second load

---
+++ Models/ViewModels/BookListViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Bookstore.Models
+{
+    public class BookListViewModel
+    {
+        public IEnumerable<Book> Books { get; set; } = new List<Book>();
+        public BookGridData CurrentRoute { get; set; } = new BookGridData();
+        public int TotalPages { get; set; }
+
+        public int[] PageSizes => new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    }
+}
